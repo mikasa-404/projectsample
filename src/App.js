e8be34044cbde3577093c3fa581f8ba3e0cc28e9
mikasa-404 <jhapriya106@gmail.com>
2024-08-07 16:03:44 +0530

    form for max freq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  const [name, setName] = React.useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData([...data, name]);
+    setName("");
+  };
+//sort map
+const sortMap=(map)=>{
+  const sortedMap = Object.keys(map).sort((a, b) => map[b] - map[a]);
+  return sortedMap;
+}
+  const getMap = (array) => {
+    const frequencyMap = array.reduce((map, item) => {
+      map[item] = map[item] ? map[item] + 1 : 1;
+      return map;
+    }, {});
+    return sortMap(frequencyMap);
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+      <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter names..." />
+      <button type="submit" id="btn">Submit</button>
+      </form>
+
+      <div className='items-container'>
+        {data.map((item, index) => (
+          <div>{item}</div>
+        ))}   
+      </div>
+      
+      <h1>
+        Result with highest frequency first
+      </h1>
+      <div className='result-container'>
+      {
+        getMap(data).map((item, index) => (
+          <li key={index}>{item}</li>
+        ))
+      }   
+      </div>
+      
+
+      
+
     </div>
   );
 }
